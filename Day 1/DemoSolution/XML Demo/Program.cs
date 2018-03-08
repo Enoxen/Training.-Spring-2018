ba91ee4f058731971df1f4ee44476c6be146521e
AnzhelikaK <anzhelika.kravchuk@gmail.com>
2018-03-08 17:17:47 +0300

    Change sample

---
+++ Day 1/DemoSolution/XML Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XML_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Triangle(1, 2, 3);
+            t.GetArea();
+            var tr = new XmlTriangle(1, 2, 5);
+            tr.GetArea();
+            Console.ReadKey();
+        }
+    }
+}
